e0561101f6ac05faf40f03112872b0538253718a
ranjitkumarpatel <ranjit.kumar-patel@klarna.com>
2021-03-10 15:01:24 +0100

    initial commit

---
+++ src/main/java/de/ranjit/jacksonexample/JacksonExampleApplication.java
@@ -0,0 +1,13 @@
+package de.ranjit.jacksonexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacksonExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/ranjit/jacksonexample/JacksonExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ranjit.jacksonexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
